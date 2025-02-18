1e387db4acffc8e312c2d32611b5bbf61f89d94c
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-02-18 12:09:51 +0100

    added pairing players logic

--- server.js
+++ server.js
@@ -15,3 +15,29 @@ const io = new Server(server); // Connecting Socket.io to the server
 
 // Serving static files from the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Pairing players
+const players = {};
+const waitingPlayer = null;
+
+io.on("connection", (socket) => {
+    console.log(`New player connected:  ${socket.id}`);
+
+    if (!waitingPlayer) {
+        waitingPlayer = socket;
+        socket.emit("waiting", "Waiting for opponent...");
+    } else {
+        const player1 = waitingPlayer;
+        const player2 = socket;
+        const room = player1.id + "#" + player2.id;
+
+        player1.join(room);
+        player2.join(room);
+
+        players[player1.id] = {opponent: player2.id, choice: null, room};
+        players[player2.id] = {opponent: player1.id, choice: null, room};
+
+        io.to(room).emit("startGame", "Game starting! Make your choice!");
+        waitingPlayer = null;
+    }
+})
