d43d45790820ddfccbc9ea43210fe62cb26b6174
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-02-21 16:30:44 +0100

    cleaned up code

--- public/js/script.js
+++ public/js/script.js
@@ -2,23 +2,30 @@ document.addEventListener("DOMContentLoaded", () => {
     // UI setup
     const usernameInput = document.getElementById("username");
     const submitUsernameButton = document.getElementById("submit-username");
-    const opponentUsernameElement = document.querySelector(".opponent-username");
+
     const countdownHeading = document.querySelector(".countdown")
+    
     const gameStatusFeedback = document.querySelector(".game-status");
     const playerChoiceFeedback = document.querySelector(".player-choice");
     const opponentChoiceFeedback = document.querySelector(".opponent-choice");
     const resultFeedback = document.querySelector(".result");
+    
+    const opponentUsernameElement = document.querySelector(".opponent-username");
+    
     const playerScoreElement = document.querySelector(".player-score");
     const opponentScoreElement = document.querySelector(".opponent-score");
     let playerScore = 0;
     let opponentScore = 0;
 
-    // const socket = io("https://your-app-name.onrender.com"); // Connect to WebSocket server
-    const socket = io(); // Run server using localhost
-    let playerChoice = null;
-
+    // Functions
     function capitalizeFirstLetter(str) {
         return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    
+    function setButtonState(enabled) { // true = enabled, false = disabled
+        document.querySelectorAll('.buttons button').forEach(button => {
+            button.disabled = !enabled;
+        });
     }
 
     function updateUI(element, message) {
@@ -28,32 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
     function playGame(choice) {
         playerChoice = choice;
         socket.emit("playerChoice", choice); // Send choice to server
-        updateUI(gameStatusFeedback, "Waiting for opponent...");
     }
-
-    submitUsernameButton.addEventListener("click", () => {
-        const username = usernameInput.value.trim();
-        if (username) {
-            socket.emit("setUsername", username);
-            document.querySelectorAll('.buttons button').forEach(button => button.disabled = false);
-            submitUsernameButton.disabled = true;
-            usernameInput.disabled = true;
-            submitUsernameButton.style.display = 'none';
-        }
-    });
     
-    function disableButtons(disable) {
-        document.querySelectorAll('.buttons button').forEach(button => {
-        button.disabled = disable;
-        });
-    }
-
-    function enableButtons(enable) {
-        document.querySelectorAll('.buttons button').forEach(button => {
-        button.disabled = enable;
-        });
-    }
-
     function updateScore(result) {
         if (result === "You won!") {
             playerScore++;
@@ -64,19 +47,21 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    // socket.io
+    // const socket = io("https://your-app-name.onrender.com"); // Connect to WebSocket server
+    const socket = io(); // Run server using localhost
+    let playerChoice = null;
+    
     socket.on("waiting", (message) => {
         updateUI(gameStatusFeedback, message);
-        disableButtons(false); // Keep buttons enabled while waiting for opponent
     });
     
     socket.on("startGame", (message) => {
         updateUI(gameStatusFeedback, message);
-        disableButtons(false);
     });
     
     socket.on("waitingForChoice", (message) => {
         updateUI(gameStatusFeedback, message);
-        disableButtons(true); // Disable buttons while waiting for opponent's choice
     });
 
     // Alert when opponent leaves the game.
@@ -84,46 +69,45 @@ document.addEventListener("DOMContentLoaded", () => {
         updateUI(gameStatusFeedback, message);
     });
 
-    // Event listeners for player choices
-    document.getElementById("rock").addEventListener("click", () => playGame("rock"));
-    document.getElementById("paper").addEventListener("click", () => playGame("paper"));
-    document.getElementById("scissors").addEventListener("click", () => playGame("scissors"));
+    socket.on("opponentDisconnected", (message) => {
+        updateUI(gameStatusFeedback, message);
+    });
 
     socket.on("opponentUsername", (username) => {
         updateUI(opponentUsernameElement, `You're playing against: ${username}`);
     });
 
-// Receive game result from server
-socket.on("gameResult", (data) => {
-    console.log("Received gameResult event:", data);
-    const playerData = socket.id === data.player1.id ? data.player1 : data.player2;
-    console.log("Player data:", playerData);
-    updateUI(playerChoiceFeedback, "You chose: " + capitalizeFirstLetter(playerData.choice));
-    updateUI(opponentChoiceFeedback, "Opponent chose: " + capitalizeFirstLetter(playerData.opponentChoice));
+    // Receive game result from server
+    socket.on("gameResult", (data) => {
+        console.log("Received gameResult event:", data);
+        const playerData = socket.id === data.player1.id ? data.player1 : data.player2;
+        console.log("Player data:", playerData);
+        updateUI(playerChoiceFeedback, "You chose: " + capitalizeFirstLetter(playerData.choice));
+        updateUI(opponentChoiceFeedback, "Opponent chose: " + capitalizeFirstLetter(playerData.opponentChoice));
 
-    switch (playerData.result) {
-        case "You won!":
-            resultFeedback.classList.add("winner");
-            resultFeedback.classList.remove("draw");
-            resultFeedback.classList.remove("loser");
-            break;
+        switch (playerData.result) {
+            case "You won!":
+                resultFeedback.classList.add("winner");
+                resultFeedback.classList.remove("draw");
+                resultFeedback.classList.remove("loser");
+                break;
 
-        case "You lost!":
-            resultFeedback.classList.add("loser");
-            resultFeedback.classList.remove("draw");
-            resultFeedback.classList.remove("winner");
-            break;
+            case "You lost!":
+                resultFeedback.classList.add("loser");
+                resultFeedback.classList.remove("draw");
+                resultFeedback.classList.remove("winner");
+                break;
 
-        case "Draw!":
-            resultFeedback.classList.add("draw");
-            resultFeedback.classList.remove("winner");
-            resultFeedback.classList.remove("loser");
-            break;
-    }
-   
-    updateUI(resultFeedback, playerData.result);
-    updateScore(playerData.result);
-});
+            case "Draw!":
+                resultFeedback.classList.add("draw");
+                resultFeedback.classList.remove("winner");
+                resultFeedback.classList.remove("loser");
+                break;
+        }
+    
+        updateUI(resultFeedback, playerData.result);
+        updateScore(playerData.result);
+    });
 
     socket.on("roundComplete", (message) => {
         let countdown = 5; // 5 seconds countdown
@@ -137,8 +121,23 @@ socket.on("gameResult", (data) => {
             } else {
                 clearInterval(countdownInterval);
                 updateUI(countdownHeading, message);
-                enableButtons();
             }
         }, 1000); // Update every second
+    });
+
+    // Event listeners
+    document.getElementById("rock").addEventListener("click", () => playGame("rock"));
+    document.getElementById("paper").addEventListener("click", () => playGame("paper"));
+    document.getElementById("scissors").addEventListener("click", () => playGame("scissors"));
+    
+    submitUsernameButton.addEventListener("click", () => {
+        const username = usernameInput.value.trim();
+        if (username) {
+            socket.emit("setUsername", username);
+            document.querySelectorAll('.buttons button').forEach(button => button.disabled = false);
+            submitUsernameButton.disabled = true;
+            usernameInput.disabled = true;
+            submitUsernameButton.style.display = 'none';
+        }
     });
 });
